7254c47f3d86be21d0da263164cad63df6b4cb1a
Dracø <sjagroop212@gmail.com>
2022-08-23 03:38:04 -0600

    Changes to index.html to php

--- index.php
+++ index.php
@@ -4,7 +4,10 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="refresh" content="0; url=php/index.php" />
-  <title>Reeedirect</title>
+  <!-- <meta http-equiv="refresh" content="0; url=php/index.php" /> -->
+  <title>Redirect</title>
 </head>
+<body>
+  <a href="php/index.php">Ref</a>
+</body>
 </html>
